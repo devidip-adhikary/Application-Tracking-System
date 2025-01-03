fecd81966f245186e57cdda559c9c233baf00a56
Devidip Adhikary <devidip.adhikary@protivitiglobal.in>
2025-01-03 17:43:28 +0530

    clients and vendors

--- Frontend/application-tracking-system/src/app/clients/page.tsx
+++ Frontend/application-tracking-system/src/app/clients/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import TableThree from "@/components/Tables/TableThree";
+import TableOne from "@/components/Tables/TableOne";
 import React from "react";
 
 const Clients: React.FC = () => {
@@ -10,7 +10,7 @@ const Clients: React.FC = () => {
       <Breadcrumb pageName="List of Clients" />
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <div className="col-span-12 xl:col-span-12">
-          <TableThree />
+          <TableOne />
         </div>
       </div>
     </DefaultLayout>
