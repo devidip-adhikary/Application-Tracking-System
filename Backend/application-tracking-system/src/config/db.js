5af3a97f5252d35855dac6a9141aad11011fdd68
Devidip Adhikary <devidip.adhikary@protivitiglobal.in>
2025-01-30 19:26:08 +0530

    chnage in db

--- Backend/application-tracking-system/src/config/db.js
+++ Backend/application-tracking-system/src/config/db.js
@@ -1,25 +1,35 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 // Initialize Sequelize
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: String(process.env.DB_SERVER),
-  dialect: 'mssql',
-  port: parseInt(process.env.DB_PORT) || 1433,
-  dialectOptions: {
-    options: {
-      enableArithAbort: true,
-      encrypt: process.env.DB_ENCRYPT === 'true', // Use encryption for Azure SQL Server
-      trustServerCertificate: true,
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: String(process.env.DB_SERVER),
+    dialect: "mssql",
+    port: parseInt(process.env.DB_PORT) || 1433,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Required for Render PostgreSQL
+      },
+
+      options: {
+        enableArithAbort: true,
+        encrypt: process.env.DB_ENCRYPT === "true", // Use encryption for Azure SQL Server
+        trustServerCertificate: true,
+      },
     },
-  },
-});
+  }
+);
 
 // Test the connection
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 };
 
